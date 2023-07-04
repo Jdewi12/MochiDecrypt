730ed654f8f3d2252bab4d9ac793722b7c6f24e5
Jack de Wilde <Jackdew31@icloud.com>
2023-07-04 23:49:12 +1000

    Initial Commit

---
+++ MochiDecrypt/Program.cs
@@ -0,0 +1,13 @@
+﻿using MochiCryptUnpacker;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        if(args.Length != 1)
+            throw new ArgumentException("Supply only one argument (binary payload file)");
+        var output = MochiDecrypt.Decrypt(File.ReadAllBytes(args[0]));
+        if(output != null)
+            File.WriteAllBytes(AppDomain.CurrentDomain.BaseDirectory + "/output.swf", output);
+    }
+}
